2680a4a8c766dd2412efd23a96630686dfbbef3a
Ethan Glover <glover.ethan@gmail.com>
2020-01-03 16:26:55 -0600

    If a nonprofit is removed from the database, but is in a users favorites list, the user will get a message so on their list.

--- frontend/components/Favorite.jsx
+++ frontend/components/Favorite.jsx
@@ -3,15 +3,25 @@ import PropTypes from 'prop-types';
 import RemoveFromFavorites from './RemoveFromFavorites';
 
 
-const Favorite = ({ favorite }) => (
-	<>
-		<div>{favorite.nonProfit.ein} - {favorite.nonProfit.name}
-			<RemoveFromFavorites id={favorite.id} /></div>
-	</>
-)
+const Favorite = ({favorite}) => {
+    // first check if nonProfit exists
+    if (!favorite.nonProfit) return (
+        <>
+            <p>This non-profit has been removed from the database.</p>
+            <RemoveFromFavorites id={favorite.id}/>
+        </>
+    );
+
+    return (
+        <>
+            <div>{favorite.nonProfit.ein} - {favorite.nonProfit.name}
+                <RemoveFromFavorites id={favorite.id}/></div>
+        </>
+    )
+};
 
 Favorite.propTypes = {
-	favorite: PropTypes.object.isRequired,
+    favorite: PropTypes.object.isRequired,
 };
 
 export default Favorite;
