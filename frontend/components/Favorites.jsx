f8d82d5624963901e6c226f57b112363a54a7daf
Ethan Glover <glover.ethan@gmail.com>
2020-01-03 16:39:18 -0600

    React-adopt addition to Favorites component. Testing only.

--- frontend/components/Favorites.jsx
+++ frontend/components/Favorites.jsx
@@ -1,24 +1,29 @@
 import React from "react";
-import { Query, Mutation } from "react-apollo";
-import gql from "graphql-tag";
+import {adopt} from 'react-adopt';
 import User from './User';
 import Favorite from './Favorite';
+
+const Composed = adopt({
+    user: ({render}) => <User>{render}</User>,
+});
 
 const Favorites = () => {
     return (
         <>
-            <User>{({ data: { me } }) => {
+            <Composed>{({user}) => {
+                const me = user.data.me;
                 if (!me) return null;
                 return (
                     <div>
                         <h3>{me.name}'s Favorites</h3>
-                        <p>You Have {me.favorites.length} {me.favorites.length === 1 ? 'Charity' : 'Charities'} In Your Favorites</p>
+                        <p>You Have {me.favorites.length} {me.favorites.length === 1 ? 'Charity' : 'Charities'} In Your
+                            Favorites</p>
                         <ul>
-                            {me.favorites.map(favorite => <Favorite key={favorite.id} favorite={favorite} />)}
+                            {me.favorites.map(favorite => <Favorite key={favorite.id} favorite={favorite}/>)}
                         </ul>
                     </div>
                 )
-            }}</User>
+            }}</Composed>
         </>
     )
 };
