1697df7855691f870fe913cc98d74fb6bd8f9446
Ethan Glover <glover.ethan@gmail.com>
2020-01-03 19:56:49 -0600

    Added search functionality for nonprofits.

--- frontend/components/Search.jsx
+++ frontend/components/Search.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import Downshift from "downshift";
+import Downshift, {resetIdCounter} from "downshift";
 import Router from "next/router";
 import {ApolloConsumer} from 'react-apollo';
 import gql from 'graphql-tag';
 import debounce from 'lodash.debounce';
+import {DropDown, DropDownItem, SearchStyles} from './styles/DropDown'
 
 const SEARCH_NONPROFITS_QUERY = gql`
-   query SEARCH_NONPROFITS_QUERY($searchTerm: String!) {
-       nonProfits(where: {
-           OR: [
-              { name_contains: $searchTerm},
-           ]
-       }) {
-           id
-           name
-       }
-   } 
+    query SEARCH_NONPROFITS_QUERY($searchTerm: String!) {
+        nonProfits(where: {
+            OR: [
+                { name_contains: $searchTerm},
+            ]
+        }) {
+            id
+            name
+        }
+    }
 `;
+
+function routeToNonProfit(nonProfit) {
+    Router.push({
+        pathname: '/nonProfit',
+        query: {
+            id: nonProfit.id,
+        },
+    });
+}
 
 class AutoComplete extends React.Component {
 
@@ -41,28 +51,60 @@ class AutoComplete extends React.Component {
     }, 350);
 
     render() {
+        resetIdCounter();
         return (
-            <div>
-                <ApolloConsumer>
-                    {(client) => (
-                        <input
-                            type="search"
-                            onChange={(e) => {
-                                e.persist();
-                                this.onChange(e, client);
-                            }}
-                        />
-                    )}
-                </ApolloConsumer>
-                <>
-                    {this.state.nonProfits.map(nonProfit =>
-                        <div key={nonProfit.id}>
-                            {nonProfit.name}
+            <SearchStyles>
+                <Downshift
+                    onChange={routeToNonProfit}
+                    itemToString={nonProfit => (nonProfit === null ? '' : nonProfit.name)}
+                >
+                    {({getInputProps, getItemProps, isOpen, inputValue, highlightedIndex}) => (
+                        <div>
+                            <ApolloConsumer>
+                                {client => (
+                                    <input
+                                        {...getInputProps({
+                                            type: 'search',
+                                            placeholder: 'Search for a Charity',
+                                            id: 'search',
+                                            className: this.state.loading ? 'loading' : '',
+                                            onChange: e => {
+                                                e.persist();
+                                                this.onChange(e, client);
+                                            },
+                                        })}
+                                    />
+                                )}
+                            </ApolloConsumer>
+                            {isOpen && (
+                                <>
+                                    {this.state.nonProfits.map((nonProfit, index) => (
+                                        <div
+                                            {...getItemProps({
+                                                key: nonProfit.id,
+                                                index,
+                                                item: nonProfit,
+                                                style: {
+                                                  backgroundColor:
+                                                    highlightedIndex === index ? 'lightgray' : 'white',
+                                                    fontSize: 16,
+                                                },
+                                            })}
+                                        >
+                                            {nonProfit.name}
+                                        </div>
+                                    ))}
+                                    {!this.state.nonProfits.length
+                                        && !this.state.loading
+                                        && <div>Nothing Found for {inputValue}</div>
+                                    }
+                                </>
+                            )}
                         </div>
                     )}
-                </>
-            </div>
-        )
+                </Downshift>
+            </SearchStyles>
+        );
     }
 }
 
