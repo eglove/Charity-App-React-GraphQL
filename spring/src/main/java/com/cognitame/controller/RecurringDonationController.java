c733924ab5b4a87deff14b19d36e475f11891df0
Ethan Glover <glover.ethan@gmail.com>
2020-02-07 16:58:58 -0600

    Spring additions

--- spring/src/main/java/com/cognitame/controller/RecurringDonationController.java
+++ spring/src/main/java/com/cognitame/controller/RecurringDonationController.java
@@ -7,13 +7,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/rest/recurringdonations")
 @RestController
+@CrossOrigin("http://localhost:7777")
 public class RecurringDonationController {
 	private static Logger logger = LoggerFactory.getLogger(RecurringDonationController.class);
 	private GraphQLService graphQLService;
@@ -23,7 +21,7 @@ public class RecurringDonationController {
 		this.graphQLService = graphQLService;
 	}
 	
-	@PostMapping
+	@GetMapping
 	public ResponseEntity<Object> getAllRecurringDonations(@RequestBody String query) {
 		logger.info("Entering getAllRecurringDonations@RecurringDonationController");
 		ExecutionResult execute = graphQLService.getGraphQL().execute(query);
