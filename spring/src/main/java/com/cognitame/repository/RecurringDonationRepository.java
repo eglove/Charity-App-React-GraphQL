2dcd97a77c1cd50b0a429381db8a664a304aa44d
eglove <hello@ethang.email>
2020-10-03 18:57:56 -0500

    delete spring

--- spring/src/main/java/com/cognitame/repository/RecurringDonationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cognitame.repository;
-
-import com.cognitame.model.RecurringDonation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecurringDonationRepository extends JpaRepository<RecurringDonation, String> {
-}
